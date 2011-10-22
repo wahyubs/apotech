2d11ede7bf1b0a768c57217e6d7c963e333bdd28
wahyu <wahyu@wahyu-PC>
2011-10-22 18:01:26 +0700

    stok

--- app/controllers/PembelianControl.java
+++ app/controllers/PembelianControl.java
@@ -2,10 +2,15 @@ package controllers;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import models.DetilPembelian;
+import models.DetilTransferStok;
 import models.ObatAlat;
 import models.Pembelian;
 import models.StokObatAlat;
@@ -28,12 +33,12 @@ public class PembelianControl extends Controller {
 		render(pembelian, hasil);
 	}
 
-	public static void transfer(TransferStok transfer) {
+	public static void transfer(TransferStok transfer, String hasil) {
 		if (transfer == null)
 			transfer = new TransferStok();
 		if (transfer.getTglTransfer() == null)
 			transfer.setTglTransfer(new Date());
-		render(transfer);
+		render(transfer, hasil);
 	}
 
 	public static void monitoring(Date tglPembelianAwal,
@@ -167,7 +172,99 @@ public class PembelianControl extends Controller {
 		renderJSON(data, String.class);
 	}
 
-	public static void saveTransfer() {
-		System.out.println("tesz");
+	public static void saveTransfer(String idTransfer,
+			@Required @As("dd-MM-yyyy") Date tglTransfer, String descTransfer,
+			List<String> key_kode_obat, List<Integer> kirim) {
+		TransferStok transferStok = new TransferStok();
+		transferStok.setTglTransfer(tglTransfer);
+		if (transferStok.getTglTransfer() == null)
+			transferStok.setTglTransfer(new Date());
+		transferStok.setDescTransfer(descTransfer);
+		if (CommonUtil.isEmpty(idTransfer)) {
+			transferStok.validateAndSave();
+		} else {
+			transferStok.setIdTransfer(idTransfer);
+			transferStok = transferStok.merge();
+			transferStok.validateAndSave();
+			DetilTransferStok.delete("id_transfer=?", idTransfer);
+		}
+		if (validation.hasErrors()) {
+			params.flash(); // add http parameters to the flash scope
+			validation.keep(); // keep the errors for the next request
+			transfer(transferStok, null);
+			return;
+		}
+		// for (int i = 0; i < key_kode_obat.size(); i++) {
+		// if (key_kode_obat.get(i) != null
+		// && !"".equals(key_kode_obat.get(i))) {
+		// Integer jmlKirim = kirim.get(i) == null ? 0
+		// : kirim.get(i);
+		// DetilPembelian detilPembelian = new DetilPembelian();
+		// StokObatAlat stokObatAlat = new StokObatAlat();
+		// stokObatAlat.setIdObatAlat((ObatAlat) ObatAlat
+		// .findById(key_kode_obat.get(i)));
+		// stokObatAlat.setTglKadaluarsa(tglKadaluarsa.get(i));
+		// List<StokObatAlat> fetch = StokObatAlat
+		// .find("id_obat_alat=? and date_trunc('day', tgl_kadaluarsa)=?",
+		// stokObatAlat.getIdObatAlat().getIdObatAlat(),
+		// stokObatAlat.getTglKadaluarsa()).fetch();
+		// if (fetch.isEmpty()) {
+		// stokObatAlat.setJmlStokApotek(0);
+		// stokObatAlat.setJmlStokGudang(0);
+		// stokObatAlat.validateAndSave();
+		// if (validation.hasErrors()) {
+		// params.flash(); // add http parameters to the flash
+		// // scope
+		// validation.keep(); // keep the errors for the next
+		// // request
+		// transaksi(pembelian, null);
+		// return;
+		// }
+		// } else {
+		// StokObatAlat tmp = fetch.get(0);
+		// stokObatAlat.setIdStok(tmp.getIdStok());
+		// Integer jmlStokApotek = tmp.getJmlStokApotek() == null ? 0
+		// : tmp.getJmlStokApotek();
+		// Integer jmlStokGudang = tmp.getJmlStokGudang() == null ? 0
+		// : tmp.getJmlStokGudang();
+		// stokObatAlat.setJmlStokApotek(jmlStokApotek);
+		// stokObatAlat.setJmlStokGudang(jmlStokGudang);
+		// stokObatAlat = stokObatAlat.merge();
+		// stokObatAlat.validateAndSave();
+		// }
+		// detilPembelian.setIdPembelian(pembelian);
+		// detilPembelian.setIdStok(stokObatAlat);
+		// detilPembelian.setTglKadaluarsa(tglKadaluarsa.get(i));
+		// detilPembelian.setJmlPenerimaanApotek(jmlTerimaApotek);
+		// detilPembelian.setJmlPenerimaanGudang(jmlTerimaGudang);
+		// detilPembelian.validateAndSave();
+		// pembelian.addDetilPembelianIdPembelian(detilPembelian);
+		// }
+		// }
+		// String hasil = "Pembelian Berhasil Disimpan!";
+		// pembelian
+		// .getIdSupplier()
+		// .setNamaSupplier(
+		// (pembelian.getIdSupplier().getNamaSupplier() != null ? pembelian
+		// .getIdSupplier().getNamaSupplier() : "")
+		// + " ("
+		// + (pembelian.getIdSupplier().getKotaSupplier() != null ? pembelian
+		// .getIdSupplier().getKotaSupplier()
+		// : "-") + ")");
+		// renderTemplate("PembelianControl/transaksi.html", pembelian, hasil);
+	}
+
+	public static void showStok(String idObatAlat) {
+		List<StokObatAlat> fetch = StokObatAlat.find("id_obat_alat=?",
+				idObatAlat).fetch();
+		final List<AutocompleteValue> response = new ArrayList<AutocompleteValue>();
+		for (Iterator iterator = fetch.iterator(); iterator.hasNext();) {
+			StokObatAlat stokObatAlat = (StokObatAlat) iterator.next();
+			response.add(new AutocompleteValue(stokObatAlat.getIdStok(), "exp:"
+					+ stokObatAlat.getTglKadaluarsa() + ",jmlA:"
+					+ stokObatAlat.getJmlStokApotek() + ",jmlG:"
+					+ stokObatAlat.getJmlStokGudang()));
+		}
+		renderJSON(response);
 	}
 }
