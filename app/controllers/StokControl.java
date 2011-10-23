085b14b14307f0d462db844ded9eed5a665c28ae
april <aprilia_03@ymail.com>
2011-10-23 23:25:31 +0700

    qty sblumnya

--- app/controllers/StokControl.java
+++ app/controllers/StokControl.java
@@ -84,6 +84,8 @@ public class StokControl extends Controller {
 						.find("id_obat_alat=? and date_trunc('day', tgl_kadaluarsa)=?",
 								stokObatAlat.getIdObatAlat().getIdObatAlat(),
 								stokObatAlat.getTglKadaluarsa()).fetch();
+				Integer jmlStokApotek = 0;
+				Integer jmlStokGudang = 0;
 				if (fetch.isEmpty()) {
 					stokObatAlat.setJmlStokApotek(0);
 					stokObatAlat.setJmlStokGudang(0);
@@ -99,20 +101,24 @@ public class StokControl extends Controller {
 				} else {
 					StokObatAlat tmp = fetch.get(0);
 					stokObatAlat.setIdStok(tmp.getIdStok());
-					Integer jmlStokApotek = tmp.getJmlStokApotek() == null ? 0
+					jmlStokApotek = tmp.getJmlStokApotek() == null ? 0
 							: tmp.getJmlStokApotek();
-					Integer jmlStokGudang = tmp.getJmlStokGudang() == null ? 0
+					jmlStokGudang = tmp.getJmlStokGudang() == null ? 0
 							: tmp.getJmlStokGudang();
 					stokObatAlat.setJmlStokApotek(jmlStokApotek);
 					stokObatAlat.setJmlStokGudang(jmlStokGudang);
 					stokObatAlat = stokObatAlat.merge();
 					stokObatAlat.validateAndSave();
 				}
+				Integer totalStok = jmlStokApotek + jmlStokGudang;
 				detilOpname.setIdStokOpname(stokOpname);
 				detilOpname.setIdStok(stokObatAlat);
+				detilOpname.setJmlApotekSebelumnya(jmlStokApotek);
 				detilOpname.setJmlApotekSekarang(jmlApotekSekarang);
 				detilOpname.setTglKadaluarsa(tglKadaluarsa.get(i));
+				detilOpname.setJmlGudangSebelumnya(jmlStokGudang);
 				detilOpname.setJmlGudangSekarang(jmlGudangSekarang);
+				detilOpname.setJmlSebelumnya(totalStok);
 				detilOpname.setJmlSekarang(jmlSekarang);
 				detilOpname.validateAndSave();
 				stokOpname.addDetilOpnameIdStokOpname(detilOpname);				
@@ -156,4 +162,21 @@ public class StokControl extends Controller {
 		renderTemplate("StokControl/transaksi.html", stokOpname, hasil);
 		
 	}
+	
+	public static void showQtySblmnya(String idObatAlat, @As("dd-MM-yyyy") Date tglKadaluarsa) {
+		List<StokObatAlat> fetch = StokObatAlat.find("id_obat_alat=? and date_trunc('day', tgl_kadaluarsa)=?",
+				idObatAlat,tglKadaluarsa).fetch();
+		final List data = new ArrayList();
+		for (Iterator iterator = fetch.iterator(); iterator.hasNext();) {
+			StokObatAlat stokObatAlat = (StokObatAlat) iterator.next();			
+			Integer jmlStokApotek = stokObatAlat.getJmlStokApotek();
+			Integer jmlStokGudang = stokObatAlat.getJmlStokGudang();
+			Integer totalStok = jmlStokApotek+jmlStokGudang;
+			data.add(jmlStokApotek);
+			data.add(jmlStokGudang);
+			data.add(totalStok);
+		}
+		renderJSON(data);
+	}
+	
 }
